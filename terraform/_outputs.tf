b3ea1880a5e69847f2acdae6c0b00d558ff34786
Chisel <chisel@malik-wright.uk>
2023-05-13 12:39:22 +0100

    fix typo: project_verion -> project_version

--- terraform/_outputs.tf
+++ terraform/_outputs.tf
@@ -3,6 +3,6 @@ output "commit_version" {
   value = data.external.useful_version_info.result.commit_version
 }
 
-output "project_verion" {
+output "project_version" {
   value = "${data.external.useful_version_info.result.project_dir}:${data.external.useful_version_info.result.commit_version}"
 }
