e322bfd0b0f7f58d33ffa571a17a14b81312edcc
Chisel <chisel@malik-wright.uk>
2023-07-31 17:19:15 +0100

    fix: use distinct names for resources (data.archive_file)

--- terraform/apigw-auth.tf
+++ terraform/apigw-auth.tf
@@ -1,6 +1,6 @@
 
 # Declare the local zip archive
-data "archive_file" "lambda_code" {
+data "archive_file" "lambda_code_auth" {
   type        = "zip"
   source_file = "./lambda-src/apigw_auth.py"
   output_path = "./tmp/apigwauth.zip"
@@ -12,9 +12,9 @@ resource "aws_lambda_function" "lambda_apigw_auth" {
   # checkov:skip=CKV_AWS_117: ADD REASON
   # checkov:skip=CKV_AWS_50: ADD REASON
   function_name    = "apigw-auth"
-  filename         = data.archive_file.lambda_code.output_path
-  source_code_hash = data.archive_file.lambda_code.output_base64sha256
+  filename         = data.archive_file.lambda_code_auth.output_path
+  source_code_hash = data.archive_file.lambda_code_auth.output_base64sha256
   role             = data.aws_iam_role.iam_for_lambda.arn
   handler          = "apigw_auth.lambda_handler"
-  runtime          = "python3.11"
+  runtime          = "python3.10"
 }
