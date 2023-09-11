88dff42d6b2a0b767794ba5408bd8fcc2fb58c59
Chisel <chisel@malik-wright.uk>
2023-09-11 20:56:35 +0100

    remove modules/api_gateway
    we're trying to easier way with an openapi spec

--- terraform/rest_api.tf
+++ terraform/rest_api.tf
@@ -5,22 +5,22 @@ module "core_lambda_function" {
   s3_bucket_name       = "${local.account_id}-${local.aws_default_region}-lambda-${local.pdf_api_base_name}"
 }
 
-module "api_gateway" {
-  source = "./modules/api_gateway"
+# module "api_gateway" {
+#   source = "./modules/api_gateway"
 
-  rest_api_name             = local.pdf_api_base_name
-  rest_api_description      = local.pdf_api_description
-  api_gateway_region        = local.aws_default_region
-  api_gateway_account_id    = local.account_id
-  lambda_function_base_name = module.core_lambda_function.base_function_name
-  scripts_invoke_arn        = module.core_lambda_function.scripts_invoke_arn
-  status_invoke_arn         = module.core_lambda_function.status_invoke_arn
-  current_version           = "${data.external.useful_version_info.result.project_dir}:${data.external.useful_version_info.result.commit_version}"
-  depends_on = [
-    module.core_lambda_function
-  ]
-}
+#   rest_api_name             = local.pdf_api_base_name
+#   rest_api_description      = local.pdf_api_description
+#   api_gateway_region        = local.aws_default_region
+#   api_gateway_account_id    = local.account_id
+#   lambda_function_base_name = module.core_lambda_function.base_function_name
+#   scripts_invoke_arn        = module.core_lambda_function.scripts_invoke_arn
+#   status_invoke_arn         = module.core_lambda_function.status_invoke_arn
+#   current_version           = "${data.external.useful_version_info.result.project_dir}:${data.external.useful_version_info.result.commit_version}"
+#   depends_on = [
+#     module.core_lambda_function
+#   ]
+# }
 
-output "core_lambda_function_arn" {
-  value = module.core_lambda_function.function_arn
-}
+# output "core_lambda_function_arn" {
+#   value = module.core_lambda_function.function_arn
+# }
